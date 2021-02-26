0cc00f4c646b920c1a46de628b94c2b108e20610
hermes.herrera <hermes@23people.io>
2021-02-26 11:03:35 -0300

    fix: ts error

--- document-pipeline/bin/document-pipeline.ts
+++ document-pipeline/bin/document-pipeline.ts
@@ -5,5 +5,5 @@ import { DocumentPipelineStack } from "../lib/document-pipeline-stack";
 
 const ENV = process.env.ENV || "Dev";
 const app = new cdk.App();
-
+const props = {};
 new DocumentPipelineStack(app, "DocumentPipelineStack" + ENV, props);
